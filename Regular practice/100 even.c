ef109ad93ccb476f0f0692224fbd6401259fb33a
mishuk09 <mishukinfo09@gmail.com>
2022-11-18 12:04:44 -0500

    c regular practice

---
+++ Regular practice/100 even.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=2;i<=200;i+=2)
+        printf("%4d\n",i);
+
+
+
+}
---
+++ Regular practice/27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int mul,sum,num[]={10,10,10,10,10};
+
+     sum=num[0]+num[1]+num[2]+num[3]+num[4];
+     mul=num[0]*num[1]*num[2]*num[3]*num[4];
+    printf("%d\n",sum);
+    printf("%d\n",mul);
+
+
+}
