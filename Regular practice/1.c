c62d390a99d8ac589be1511cee618fcbb559a7f5
mishuk09 <mishukinfo09@gmail.com>
2022-11-20 11:49:30 -0500

    c regular practice

---
+++ Regular practice/1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+
+    int num1,num2,sum;
+    printf("Enter two value=");
+    scanf("%d %d",&num1,&num2);
+    sum=num1+num2;
+
+    printf("Sum=%d",sum);
+    getch();
+
+
+
+
+
+
+}
---
+++ Regular practice/10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float c,f;
+    printf("Enter value of centregate=");
+    scanf("%f",&c);
+    f=(c*1.8)+32;
+    printf("F is=%.2f",f);
+    getch();
+
+
+
+}
