6475a8452d00750bb893ee2b0cfb4ede16c64734
mishuk09 <mishukinfo09@gmail.com>
2022-11-20 11:47:36 -0500

    c regular practice

---
+++ Regular practice/14.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter any number=");
+    scanf("%d",&num);
+    printf("the value of data type is =%d",num);
+
+    getch();
+
+}
---
+++ Regular practice/9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+
+    float height,width,area;
+    printf("Enter height and width=");
+    scanf("%f %f",&height,&width);
+    area=(height*width);
+    printf("Area is=%.2f",area);
+    getch();
+
+}
